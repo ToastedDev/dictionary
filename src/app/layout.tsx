e36491a7540d45d78edd6198f29d52cbaf3f5c19
Not TCA <realtcatech@gmail.com>
2023-05-11 15:01:01 +0000

    add title template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,10 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Toasted Dictionary",
+  title: {
+    default: "Toasted Dictionary",
+    template: "%s | Toasted Dictionary",
+  },
   description: "Your one-stop-shop for any toasted words.",
   themeColor: "#ff7d00",
 };
